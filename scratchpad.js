3af9006158540ecc6767590d56db7bd640ab03b3
Chauhan <chahcha@amazon.com>
2022-07-17 22:49:00 -0700

    added colon, dash and dot format

---
+++ scratchpad.js
@@ -0,0 +1,4 @@
+let arr = ["apple", "banana", "lychee"]
+for (const [idx, val] of arr.entries()){
+    console.log(idx, val)
+}
--- static/js/main.js
+++ static/js/main.js
@@ -4,9 +4,38 @@
 
     let updateMacTable = (event = null) => {
         const macAddrInputElemValue = event ? event.target.value : macAddrInputElem.value
-        let macAddrNoSpecialChar = macAddrInputElemValue.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
-        document.querySelector("#macLower").innerText = macAddrNoSpecialChar
-        document.querySelector("#macUpper").innerText = macAddrNoSpecialChar.toUpperCase()
+        let mac = macAddrInputElemValue.replace(/[^a-fA-F0-9]/g, "").toLowerCase()
+        
+        let colonFormat = ""
+        let dashFormat = ""
+        let dotFormat = ""
+        
+        for (const i in mac) {
+            colonFormat += mac[i]
+            dashFormat += mac[i]
+            dotFormat += mac[i]
+            
+            if (i != 0 && i != 11 && i % 2 != 0) {
+                colonFormat += ":"
+                dashFormat += "-"
+            }
+
+            if (i != 0 && i != 11 && i % 4 == 3) {
+                dotFormat += "."
+            }
+            
+            
+        }
+        
+        document.querySelector("#macLower").innerText = mac
+        document.querySelector("#macUpper").innerText = mac.toUpperCase()
+        document.querySelector("#macColon").innerText = colonFormat
+        document.querySelector("#macColonUpper").innerText = colonFormat.toUpperCase()
+        document.querySelector("#macDash").innerText = dashFormat
+        document.querySelector("#macDashUpper").innerText = dashFormat.toUpperCase()
+        document.querySelector("#macDot").innerText = dotFormat
+        document.querySelector("#macDotUpper").innerText = dotFormat.toUpperCase()
+        
     }
 
     if (macAddrInputElem.value) {
